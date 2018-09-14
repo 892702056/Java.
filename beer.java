7e458e973ce98133f5b338904e24c54397fe1001
892702056 <43234227+892702056@users.noreply.github.com>
2018-09-14 23:58:48 +0800

    Add files via upload

--- beer.java
+++ beer.java
@@ -1,11 +1,15 @@
 public class beer{
    public static void main(String[]args){
-         int  num=99;	
+         int num=99;	
 		 for(int i=num;i>=0;i--)
 		{
-			if(i==2||i==1)
+			if(i==2)
 			{
 				System.out.println(i+"bottles of beer on the wall,"+i+"bottles of beer.\nTake one down\nPass it around \n"+(i-1)+"bottle of beer on the wall\n");
+			}
+			else if(i==1){
+				
+				System.out.println(i+"bottle of beer on the wall,"+i+"bottles of beer.\nTake one down\nPass it around \n"+(i-1)+"bottle of beer on the wall\n");
 			}
 	        else if(i==0)
 			{
@@ -16,14 +20,8 @@ public class beer{
 				
 				System.out.println(i+"bottles of beer on the wall,"+i+"bottles of beer.\nTake one down\nPass it around "+(i-1)+"bottles of beer on the wall");
 				
-			}
-			
-			
-		} 
-	   
-	 
-	   
-	   
+			}	
+		}
    }
 
 
