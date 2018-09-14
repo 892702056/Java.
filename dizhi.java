54932b69a0368824102c39e6026f8fb2cf4a56d3
892702056 <43234227+892702056@users.noreply.github.com>
2018-09-14 19:54:50 +0800

    Update dizhi.java

--- dizhi.java
+++ dizhi.java
@@ -3,8 +3,8 @@ public class dizhi{
    public static void main ( String []args)
    {
    
-      String a[]=new String[]{"123","234","56"};
-	  System.out.println(a);
+      String a[]=new String[]{"123","234","56"};//创建一个字符串数组对象，将字符串填入其中；
+	  System.out.println(a);//打印a 输出的是其地址。
    
    }
 
